e4c8e0ccb7787c930d081928084d11b013010bc7
Lam Teng <lamteng@hotmail.com>
2021-02-02 22:39:18 +0000

    added new entities

--- tests/AppBundle/Entity/DinosaurTest.php
+++ tests/AppBundle/Entity/DinosaurTest.php
@@ -1,14 +1,25 @@
 <?php
+/**
+ * php vendor/phpunit/phpunit/phpunit
+ */
 
 namespace Tests\AppBundle\Entity;
 
+use AppBundle\Entity\Dinosaur;
 use PHPUnit\Framework\TestCase;
 
 class DinosaurTest extends TestCase
 {
     public function testThatYourComputerWorks()
     {
-        $this->assertTrue(false);
+        $this->assertTrue(true);
+    }
+    public function testSettingLength()
+    {
+        $dinosaur = new Dinosaur();
+        $this->assertSame(0, $dinosaur->getLength());
+        $dinosaur->setLength(9);
+        $this->assertSame(9, $dinosaur->getLength());
     }
 }
-}
+
